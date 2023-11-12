cd0f75819096d4c5b74d98accc60c0573a89d6f9
idjotherwise <ifan.johnston@gmail.com>
2023-11-12 23:01:35 +0000

    Remove spinner and constrain player

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Ninja Killers 10".to_string(),
+                canvas: Some("#bevy".to_owned()),
+                // The canvas size is constrained in index.html and build/web/styles.css
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
                 ..default()
             }),
             ..default()
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
 use crate::{actions::Actions, loading::TextureAssets, GameState};
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 pub struct PlayerPlugin;
 
@@ -18,7 +18,7 @@ fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
     commands
         .spawn(SpriteBundle {
             transform: Transform::from_translation(Vec3::new(0., 0., 1.))
-                .with_scale(Vec3::new(4., 4., 1.)),
+                .with_scale(Vec3::new(2., 2., 1.)),
             texture: textures.character.clone(),
             ..Default::default()
         })
@@ -29,17 +29,28 @@ fn move_player(
     time: Res<Time>,
     actions: Res<Actions>,
     mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     if actions.player_movement.is_none() {
         return;
     }
+
+    let window = window_query.get_single().unwrap();
+    let half_player_size = 32.;
     let speed = 150.;
     let movement = Vec3::new(
         actions.player_movement.unwrap().x * speed * time.delta_seconds(),
         actions.player_movement.unwrap().y * speed * time.delta_seconds(),
         0.,
     );
+    let x_min = -(window.width() / 2.0) + half_player_size;
+    let x_max = window.width() / 2.0 - half_player_size;
+    let y_min = -(window.height() / 2.0) + half_player_size;
+    let y_max = window.height() / 2.0 - half_player_size;
     for mut player_transform in &mut player_query {
-        player_transform.translation += movement;
+        let new_pos = player_transform.translation + movement;
+        if new_pos.x > x_min && new_pos.x < x_max && new_pos.y > y_min && new_pos.y < y_max {
+            player_transform.translation += movement;
+        }
     }
 }
